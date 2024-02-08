3cf882f06725ad2557872830fb425cb8753f05b4
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-02-08 19:24:51 +0530

    Tailwind CSS Setup and Installation

---
+++ Tailwind CSS Setup/Tailwind Installation - Post CSS/node_modules/rollup/dist/es/parseAst.js
@@ -0,0 +1,12 @@
+/*
+  @license
+	Rollup.js v4.9.1
+	Sun, 17 Dec 2023 06:25:43 GMT - commit d56ac63dc0452820272a0d7536340277f7db68bf
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+import '../native.js';
+export { parseAst, parseAstAsync } from './shared/parseAst.js';
+import 'node:path';
---
+++ Tailwind CSS Setup/Tailwind Installation - Post CSS/node_modules/rollup/dist/shared/fsevents-importer.js
@@ -0,0 +1,37 @@
+/*
+  @license
+	Rollup.js v4.9.1
+	Sun, 17 Dec 2023 06:25:43 GMT - commit d56ac63dc0452820272a0d7536340277f7db68bf
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+let fsEvents;
+let fsEventsImportError;
+async function loadFsEvents() {
+    try {
+        ({ default: fsEvents } = await import('fsevents'));
+    }
+    catch (error) {
+        fsEventsImportError = error;
+    }
+}
+// A call to this function will be injected into the chokidar code
+function getFsEvents() {
+    if (fsEventsImportError)
+        throw fsEventsImportError;
+    return fsEvents;
+}
+
+const fseventsImporter = /*#__PURE__*/Object.defineProperty({
+    __proto__: null,
+    getFsEvents,
+    loadFsEvents
+}, Symbol.toStringTag, { value: 'Module' });
+
+exports.fseventsImporter = fseventsImporter;
+exports.loadFsEvents = loadFsEvents;
+//# sourceMappingURL=fsevents-importer.js.map
