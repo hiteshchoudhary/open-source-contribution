1489de3edabc523652a35315a4b5971f545472a4
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-15 20:06:38 +0530

    wip: add queue implementation

---
+++ Data Structure Using C/queue/queue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define maxsize 10
+
+// Array implementation of a queue
+
+// You can enqueue, dequeue
+// if full print Queue during enque and Exit
+// if empty during dequeue print no itme and Exit
+// dequeue: if not empty, print head and add increment head
+// enqueue: if not full, add item to queue and increment tail
+
+
