a950161dee8bbe64df25599e85cfbcdc1da7c233
Amisha Singh <amishakumari1703@gmail.com>
2024-02-25 01:32:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,113 @@
 console.log("LMAO")
-console.log("First contribution")
+console.log("First contribution);
+            // Basics of JS
+            // 1 . difference between null and undefined data types
+
+// let a=null;
+// let b;
+// console.log(a)
+// console.log(b)
+// console.log(typeof a)
+// console.log(typeof b)
+
+// 2.  variable initialized at the time of initialization
+
+// var v;
+// let l;
+// const x=1;
+
+// 3.  
+
+// let languages='java javaScript python cSharp';
+// let result=languages.lastIndexOf('S');
+// console.log(result);
+
+
+//4
+
+// let variable = 'hello programmers';
+// let results = Number(variable);
+// console.log(results);
+
+//5
+
+// let num1 = 32;
+// let num2 = '32';
+// let result1 = num1 !== num2;
+// let result2 = num1 != num2;
+// console.log(result1, result2);
+
+
+//6
+
+// let str = 'Hello Programmers';
+// let result = str.includes('r');
+// console.log(result);
+
+
+// 7
+
+// let num1 = 2;
+// let num2 = 5;
+// let result = num1 ** num2 * 2;
+// console.log(result);
+
+//8:
+
+// let num1 = [1, 2, 4, 5];
+// let num2 = [6, 5, 8, 0];
+// let result = num1.concat(num2);
+// console.log(result);
+
+// 9:
+
+// let a = 5;
+// let b = 7;
+// let c = 8;
+// let result = a < b > c;
+// console.log(result);
+
+
+//10:
+
+// let i = 4;
+
+// for (let j = 0; i < 10; i++) {
+//   if (j === 1 || i === 6) {
+//     continue;
+//   } else {
+//     console.log(i, j);
+
+//     if (i === 7) {
+//       break;
+//     }
+//   }
+// }
+
+//11:
+
+// let i = 0;
+// for (i; i < 5; i++) {
+//   console.log(i);
+// }
+
+//12:
+
+// console.log(addtwo(5));;
+
+//  function addtwo(num){
+//     return num+2;
+// }
+
+console.log(addtwo(5));
+
+addtwo= function (num){
+    return num+2;
+}
+
+
+
+
+
+
+
