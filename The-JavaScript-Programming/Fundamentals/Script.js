4d772e11be88c6fa527d656269f4a24be7c76689
GunaPalanivel <guna.gp59@gmail.com>
2024-02-25 16:00:16 +0530

    Refactor JavaScript fundamentals code

--- The-JavaScript-Programming/Fundamentals/Script.js
+++ The-JavaScript-Programming/Fundamentals/Script.js
@@ -1,7 +1,5 @@
 // Welcome to My JavaScript File!
 
-// I've enhanced and added comments for better readability and understanding.
-
 // The JavaScript code encompasses concepts such as script tags and external file inclusion, functions, logging/printing, data types, if-else statements, loops, string manipulation, math operations, and numeric handling, providing a comprehensive overview of fundamental JavaScript features.
 
 // Declare a function to add two numbers
@@ -102,5 +100,3 @@ console.log(Number.MAX_VALUE); // Output: 1.7976931348623157e+308
 console.log(Number.MIN_VALUE); // Output: 5e-324
 console.log(Number.NEGATIVE_INFINITY); // Output: -Infinity
 console.log(Number.POSITIVE_INFINITY); // Output: Infinity
-
-// Feel free to continue enhancing and building upon this code!
