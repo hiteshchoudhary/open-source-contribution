ca9a588f67fc5456829a3f89fe8abea4f6b94abd
collsonu <collsonu.kumar@gmail.com>
2024-02-26 10:44:08 +0530

    fake hai ravi but code badhiya krta hai ckeck toh kro

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 console.log("LMAO")
+// const { default: Axios } = require('axios');
+const axios = require('axios').default;
+require('dotenv').config();
+let url = process.env.URL_API;
+console.log(url);
+class ApiService{
+    constructor(){}
+    
+    async getDataFromApi() { 
+        try {
+            let response = await axios.get(`${url}/api/data`);
+             return response.data;
+         } catch (error) {
+             console.log(error);
+         }
+    };
+  
+    // Add a new item to the database
+    addItemToDb = async function (){
+        try {
+            let response = await axios.post(`${url}/api/data`, {
+                name: "Test",
+                description: "Test"
+            });
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
