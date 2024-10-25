6c4207445160084b9d358b3e4fa3de92f5a7efd5
Rahul Kumar <rkmhs9570@gmail.com>
2024-10-25 15:54:18 +0530

    added code to find area of rectangle

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("LMAO")
+console.log("LMAO");
+
+//Find out the area of rectangle.
+
+let rectangleArea = (x, y) => {
+  return x * y;
+};
+
+console.log("Area of rectangle: " + rectangleArea(5, 3));
