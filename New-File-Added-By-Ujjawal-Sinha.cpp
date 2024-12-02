12492f355ad35f04529cc86fcbe691cbbad28e10
Ujjawal Sinha <2201cs73_ujjawal@iitp.ac.in>
2024-12-02 19:07:23 +0530

    I have added new file as my first open source contribution check

---
+++ New-File-Added-By-Ujjawal-Sinha.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        cout << endl;
+    }
+    return 0;
+}
