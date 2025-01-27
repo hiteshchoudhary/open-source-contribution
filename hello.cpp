47f3197f4b18d31d8a62e469f29899ab52d510d0
Pankaj <pyadav5Visteon@gmail.com>
2025-01-27 15:33:06 +0530

    first pr commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name = "Pankaj";
+    cout << "Hello from Learning pull request" << endl;
+    cout << "thankyou "<< name << endl; 
+}
