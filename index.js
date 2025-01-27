e450395fe7a4947c1002d4434c724e3eea1e9267
Rutuj Raut <rautrutuj123@gmail.com>
2025-01-27 16:21:36 +0530

    add code for generating random number from 1 - 100

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("LMAO")
+// Program to create random number between 1 - 100
+
+var randomNum = Math.floor(Math.random()*100 + 1);
+console.log("The random number generated : "+ randomNum);
