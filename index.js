fe70f5ea1a0e39191a528d87a2240490ee8bdf1a
AyushCV-46 <ayush4sringeri@gmail.com>
2025-02-05 16:24:36 +0530

    added a simple program

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("LMAO")
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+
+const num1 = 5;
+const num2 = 10;
+const result = addTwoNumbers(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is ${result}`);
