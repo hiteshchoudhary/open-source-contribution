62ae0a77a1a2a2c64d1a4e6c4fcdf613b5cf0232
Aditya Mohite <mohiteaditya2003@gmail.com>
2025-02-07 14:20:55 +0530

    inheritance js code added

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("LMAO")
+
+// Inheritance code taught by Hitesh Choudhary Sir 
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logme(){
+        console.log(`USERNAME is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course is added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", 'chai@gmail.co','123')
+
+chai.addCourse()
